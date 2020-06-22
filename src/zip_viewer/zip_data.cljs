bc1bc1774b6f7f73f65c326e53d120a6ace8094b
Stephen Cagle <stephen@read-line.com>
2020-06-22 12:08:53 -0700

    Add in all clojure.zip functions

--- src/zip_viewer/zip_data.cljs
+++ src/zip_viewer/zip_data.cljs
@@ -2,24 +2,50 @@
   (:require [clojure.zip :as zip]))
 
 (def action->zip-fn
-  {:left zip/left
+  {:append-child zip/append-child
+   :branch? zip/branch?
+   :children zip/children
+   :down zip/down
+   ;; :edit zip/edit
+   :end? zip/end?
+   :insert-child zip/insert-child
+   :insert-left zip/insert-left
+   :insert-right zip/insert-right
+   :left zip/left
    :leftmost zip/leftmost
+   :lefts zip/lefts
+   :make-node zip/make-node
+   :next zip/next
+   :node zip/node
+   :path zip/path
+   :prev zip/prev
+   :remove zip/remove
+   :replace zip/replace
    :right zip/right
    :rightmost zip/rightmost
-   :up zip/up
-   :down zip/down
-   :replace zip/replace
+   :rights zip/rights
+   :root zip/root
    :seq-zip zip/seq-zip
+   :up zip/up
    :vector-zip zip/vector-zip
-   :xml-zip zip/xml-zip})
+   :xml-zip zip/xml-zip
+   ;; :zipper zip/zipper
+   })
 
 (def action->positional-arguments
-  {:replace ["value"]
-   :seq-zip ["value"]
-   :vector-zip ["value"]
-   :xml-zip ["value"]})
+  {:append-child ["item"]
+   :insert-child ["item"]
+   :insert-left ["item"]
+   :insert-right ["item"]
+   :make-node ["node" "children"]
+   :replace ["node"]
+   :seq-zip ["root"]
+   :vector-zip ["root"]
+   :xml-zip ["root"]
+   :zipper ["branch?" "children" "make-node" "root"]})
 
 (def constructors
   #{:seq-zip
     :vector-zip
-    :xml-zip})
+    :xml-zip
+    :zipper})
